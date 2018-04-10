e7486e8c0d75ce28c1b43ac6992048535fe53998
Derek Miller <derek.miller@arm.com>
2018-04-10 16:42:59 -0500

    Removed the now-redundant *CCM_8 config and test

--- TESTS/mbedtls/config/main.cpp
+++ TESTS/mbedtls/config/main.cpp
@@ -118,28 +118,6 @@ void test_case_ecdhe_ecdsa_with_aes_256_cbc_sha384_ciphersuite() {
 }
 #endif
 
-#if MBED_CONF_MBEDTLS_ECDHE_ECDSA_WITH_AES_128_CCM_8
-void test_case_ecdhe_ecdsa_with_aes_128_ccm_8_ciphersuite() {
-    mbedtls_ssl_context ssl;
-    mbedtls_ssl_config conf;
-
-
-    mbedtls_ssl_init( &ssl );
-    mbedtls_ssl_config_defaults(&conf, MBEDTLS_SSL_IS_CLIENT, MBEDTLS_SSL_TRANSPORT_STREAM, 0);
-    ssl.conf = &conf;
-
-    const int *ciphersuites = ssl.conf->ciphersuite_list[ssl.minor_ver];
-
-    int found_128 = 0;
-    for( int i = 0; ciphersuites[i] != 0; i++ ) {
-        if (ciphersuites[i] == MBEDTLS_TLS_ECDHE_ECDSA_WITH_AES_128_CCM_8) {
-            found_128 = 1;
-        }
-    }
-    TEST_ASSERT_EQUAL_INT_MESSAGE(1, found_128, "ECDHE_ECDSA_WITH_AES_128_CCM_8 not found in ciphersuites");
-}
-#endif
-
 #if MBED_CONF_MBEDTLS_PSK_WITH_AES_CCM_8
 void test_case_psk_with_aes_ccm_8_ciphersuites() {
     mbedtls_ssl_context ssl;
